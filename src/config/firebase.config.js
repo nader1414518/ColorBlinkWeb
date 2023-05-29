c02a2fe230b414a930450e866357df18dac2548e
Nader Sayed <49884739+nader1414518@users.noreply.github.com>
2023-05-29 08:14:44 +0300

    Added Basic Functionality

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBH2lP6CqOUHqB4r7Wy1KRxAztlO_kBwo",
-  authDomain: "colorblinkapp.firebaseapp.com",
-  projectId: "colorblinkapp",
-  storageBucket: "colorblinkapp.appspot.com",
-  messagingSenderId: "657382187275",
-  appId: "1:657382187275:web:1ddb86436818683201020e",
-  measurementId: "G-TJGPVE8C7P"
+    apiKey: "AIzaSyDBH2lP6CqOUHqB4r7Wy1KRxAztlO_kBwo",
+    authDomain: "colorblinkapp.firebaseapp.com",
+    databaseURL: "https://colorblinkapp-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "colorblinkapp",
+    storageBucket: "colorblinkapp.appspot.com",
+    messagingSenderId: "657382187275",
+    appId: "1:657382187275:web:1ddb86436818683201020e",
+    measurementId: "G-TJGPVE8C7P"
 };
 
 // Initialize Firebase
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="main-container">
-    Home
+  <div class="main-container" :style="{ backgroundColor: currentColor }">
+    
   </div>
 </template>
 
+<style scoped>
+.main-container {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
+
 <script>
+import FirebaseRealtimeDatabaseController from '@/controllers/FirebaseRealtimeDatabaseController';
+
 export default {
   name: "HomePage",
+  data() {
+    return {
+      currentColor: "red",
+    };
+  },
+  async mounted() {
+    await this.listenToColorChange();
+  },
+  methods: {
+    async listenToColorChange() {
+      await FirebaseRealtimeDatabaseController.listenToColor((snapshot) => {
+        // console.log(snapshot.val());
+        const data = snapshot.val();
+
+        if (snapshot.exists()) {
+          if (data.color) {
+            this.currentColor = data.color;
+          }
+        }
+      });
+    }
+  },
 }
 </script>
